176d02082216aaf53f3eae69e1807947df6f44ff
CppMakeALL <dtl@localhost.localdomain>
2026-03-18 16:42:02 +0800

    add nanobind

---
+++ ThirdParty/nanobind/test/build/CMakeFiles/Aether.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Aether.cpython-39-x86_64-linux-gnu.so"
+  "Aether.pdb"
+  "CMakeFiles/Aether.dir/main.cpp.o"
+  "CMakeFiles/Aether.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Aether.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ThirdParty/nanobind/test/build/test.py
@@ -0,0 +1,7 @@
+import Aether
+
+# 测试功能
+print(Aether.add(10, 20))  # 输出 30
+g = Aether.Greeter("Test")
+print(g.greet())  # 输出 Hello, Test!
+print(g.name())
